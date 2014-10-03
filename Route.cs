ae48cd3624fb74b2da2a821daaff06310308dc23
Emily Uhde <emilyuhde@users.noreply.github.com>
2014-10-03 13:50:02 -0700

    Update Route.cs
    Added method comments.

--- Route.cs
+++ Route.cs
@@ -36,26 +36,46 @@ namespace Trains
             this.end = allStops[allStops.Count-1];
         }
 
+        /// <summary>
+        /// Returns this train route's starting town.
+        /// </summary>
+        /// <returns>starting town</returns>
         public char getStart()
         {
             return this.start;
         }
 
+        /// <summary>
+        /// Returns this train route's ending town.
+        /// </summary>
+        /// <returns>ending town</returns>
         public char getEnd()
         {
             return this.end;
         }
 
+        /// <summary>
+        /// Returns this train route's list of stops.
+        /// </summary>
+        /// <returns>list of all stops</returns>
         public List<char> getAllStops()
         {
             return this.allStops;
         }
 
+        /// <summary>
+        /// Returns this train route's total distance.
+        /// </summary>
+        /// <returns>total distance of the route</returns>
         public int getDistance()
         {
             return this.distance;
         }
 
+        /// <summary>
+        /// Convert's this train route's data into a string representation.
+        /// </summary>
+        /// <returns>string with route's data</returns>
         public string toString()
         {
             string info = "starting town: " + this.start + " | ending town: " + this.end + " | all stops on route: ";
@@ -67,6 +87,11 @@ namespace Trains
             return info;
         }
 
+        /// <summary>
+        /// Equality method comparing this train route to another, passed in train route.
+        /// </summary>
+        /// <param name="other">another TrainRoute object</param>
+        /// <returns>bool showing true if their values are the same</returns>
         public bool Equals(TrainRoute other)
         {
             if (other == null)
@@ -81,6 +106,11 @@ namespace Trains
             return true;
         }
 
+        /// <summary>
+        /// Equality method comparing this train route to another object.
+        /// </summary>
+        /// <param name="other">another TrainRoute object</param>
+        /// <returns>bool showing true if their values are the same</returns>
         public override bool Equals(Object obj)
         {
             if (obj == null)
@@ -93,7 +123,12 @@ namespace Trains
                 return Equals(route);
         }   
 
-
+        /// <summary>
+        /// Function to determine if two TrainRoute objects are equal using ==
+        /// </summary>
+        /// <param name="route1">first TrainRoute</param>
+        /// <param name="route2">second TrainRoute</param>
+        /// <returns>true if they are equal</returns>
         public static bool operator ==(TrainRoute route1, TrainRoute route2)
         {
             if ((object)route1 == null || ((object)route2) == null)
@@ -102,6 +137,12 @@ namespace Trains
             return route1.Equals(route2);
         }
 
+        /// <summary>
+        /// Function to determine if two TrainRoute objects are not equal using !=
+        /// </summary>
+        /// <param name="route1">first TrainRoute</param>
+        /// <param name="route2">second TrainRoute</param>
+        /// <returns>true if they are not equal</returns>
         public static bool operator !=(TrainRoute route1, TrainRoute route2)
         {
             if (route1 == null || route2 == null)
